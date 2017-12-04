57ba1607e6e4559e444229cb99fdee961d1878b8
Max Kolasinski <mkolas@umich.edu>
2017-12-04 01:24:46 -0500

    day  4

---
+++ 04/passwords.py
@@ -0,0 +1,23 @@
+# part 1
+
+count = 0
+
+with open("input1.txt") as file:
+	for row in file:
+		words = row.split()
+		wordset = set(words)
+		if len(words) == len(wordset):
+			count += 1
+print(count)
+
+# part 2
+
+count = 0
+
+with open("input1.txt") as file:
+	for row in file:
+		words = row.split()
+		wordset = set([''.join(sorted(x)) for x in row.split()])
+		if len(words) == len(wordset):
+			count += 1
+print(count)
