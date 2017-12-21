1263123658cdfa38878c920336bea52e08df6621
mkolas <barrelrolled@gmail.com>
2017-12-21 18:51:56 -0500

    day 21 finished

--- 21/art.py
+++ 21/art.py
@@ -1,6 +1,5 @@
-from math import ceil
-
 import copy
+from math import ceil
 
 rules = dict()
 an_art = [".#.", "..#", "###"]
@@ -35,8 +34,8 @@ def chunk_art(art):
         chunks.append(art)
         return chunks
     if len(art) % 2 == 0:
-        chunk_row = list()
         while len(art) > 0:
+            chunk_row = list()
             rows_to_chunk = art[:2]
             art = art[2:]
             while len(rows_to_chunk[0]) > 0:
@@ -47,16 +46,16 @@ def chunk_art(art):
             chunks.append(chunk_row)
         return chunks
     elif len(art) % 3 == 0:
-        chunk_row = list()
         while len(art) > 0:
+            chunk_row = list()
             rows_to_chunk = art[:3]
             art = art[3:]
             while len(rows_to_chunk[0]) > 0:
                 chunk = [rows_to_chunk[0][:3], rows_to_chunk[1][:3], rows_to_chunk[2][:3]]
                 chunk_row.append(chunk)
-                rows_to_chunk[0] = rows_to_chunk[0][2:]
-                rows_to_chunk[1] = rows_to_chunk[1][2:]
-                rows_to_chunk[2] = rows_to_chunk[2][2:]
+                rows_to_chunk[0] = rows_to_chunk[0][3:]
+                rows_to_chunk[1] = rows_to_chunk[1][3:]
+                rows_to_chunk[2] = rows_to_chunk[2][3:]
             chunks.append(chunk_row)
         return chunks
 
@@ -64,7 +63,7 @@ def chunk_art(art):
 def init_rows(size):
     return_rows = list()
     for s in range(size):
-        return_rows.append([])
+        return_rows.append("")
     return return_rows
 
 
@@ -72,9 +71,8 @@ def combine_row(chunk_row):
     return_rows = init_rows(len(rules[art_to_text(chunk_row[0])]))
     for chunk in chunk_row:
         transformed_chunk = rules[art_to_text(chunk)]
-        # print(transformed_chunk)
         for i, r in enumerate(transformed_chunk):
-            return_rows[i].extend(r)
+            return_rows[i] += r
     return return_rows
 
 
@@ -82,8 +80,17 @@ def print_art(art):
     for line in art:
         print(line, "\n")
 
+def count_hashes(art):
+    count = 0
+    for row in an_art:
+        for char in row:
+            if char is '#':
+                count += 1
+    return count
 
-with open("test.txt") as f:
+
+
+with open("input1.txt") as f:
     for row in f:
         row_items = row.split("=>")
         rule_art = text_to_art(row_items[0].strip())
@@ -108,17 +115,20 @@ with open("test.txt") as f:
             rules[art_to_text(vh_art)] = rule_target
 
 
-for x in range(5):
-    print("iteration ", x)
+for x in range(18):
     art_chunks = chunk_art(an_art)
-    # print(art_chunks)
     if len(art_chunks) == 1:
-        an_art = rules[art_to_text(art_chunks[0])]
+        new_art = rules[art_to_text(art_chunks[0])]
     else:
         new_art = list()
         for row in art_chunks:
-            new_row = combine_row(row)
-            new_art.append(new_row)
-        an_art = new_art
-    print_art(an_art)
+            new_rows = combine_row(row)
+            for nr in new_rows:
+                new_art.append(nr)
+    an_art = new_art
+    if x == 4:
+        print("Num #s after 5 iterations: ", count_hashes(an_art))
+    # print_art(an_art)
 
+
+print("Num #s after 18 iterations: ", count_hashes(an_art))
